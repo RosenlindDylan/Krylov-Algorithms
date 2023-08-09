b730c8b62670410061847aaa9dbbf916f78608f3
RosenlindDylan <35314066+RosenlindDylan@users.noreply.github.com>
2023-08-08 21:56:17 -0700

    updated comments / seperated code

--- cg.py
+++ cg.py
@@ -1,43 +1,30 @@
 import numpy as np
 
-# this just prints the matrices better
-np.set_printoptions(precision=3,suppress=True)
+def CGSolver(A, b, x, tol,itMax):
+    '''
+        Parameters:
+            A - (sparse) positive definite symmetrix matrix    
+            b - solution vector (entries generated uniformly between [0,1] in LeGallo paper)
+            x - initial guess vector, zero vector 
+            tol - desired convergence tolerance
+            itMax - maximum iterations
+        Return:
+            final guess vector (x) found once the tolerance or number of iterations is reached
 
-# b should have entries generated uniformly between [0,1] according to the paper
-def generateB(size):
-    return np.random.rand(size)
+        References:
+            legallo
+            https://www.cs.cmu.edu/~quake-papers/painless-conjugate-gradient.pdf
+            strang - files
+    '''
 
-b = generateB(10)
-print('the target vector is ' + str(b))
-
-# an example tolerance for error
-tol = 10**(-10)
-
-# A is a sparse positive definite symmetric matrix, later i'll write a method to generate random ones
-A = np.array([[1.00000000, 0.03677389, 0.00000000, 0.97869604, 0.00000000, 0.47722024, 0.00000000, 0.00000000, 0.00000000, 0.00000000],
- [0.03677389, 1.00000000, 0.00000000, 0.00000000, 0.84396722, 0.80661557, 0.00000000, 0.00000000, 0.00000000, 0.00000000],
- [0.00000000, 0.00000000, 1.00000000, 0.00000000, 0.00000000, 0.00000000, 0.18467612, 0.00000000, 0.00000000, 0.00000000],
- [0.97869604, 0.00000000, 0.00000000, 1.00000000, 0.00000000, 0.00000000, 0.00000000, 0.21125068, 0.00000000, 0.00000000],
- [0.00000000, 0.84396722, 0.00000000, 0.00000000, 1.00000000, 0.66669913, 0.00000000, 0.00000000, 0.87047764, 0.00000000],
- [0.47722024, 0.80661557, 0.00000000, 0.00000000, 0.66669913, 1.00000000, 0.00000000, 0.00000000, 0.00000000, 0.00000000],
- [0.00000000, 0.00000000, 0.18467612, 0.00000000, 0.00000000, 0.00000000, 1.00000000, 0.00000000, 0.00000000, 0.74501799],
- [0.00000000, 0.00000000, 0.00000000, 0.21125068, 0.00000000, 0.00000000, 0.00000000, 1.00000000, 0.00000000, 0.72975303],
- [0.00000000, 0.00000000, 0.00000000, 0.00000000, 0.87047764, 0.00000000, 0.00000000, 0.00000000, 1.00000000, 0.78071397],
- [0.00000000, 0.00000000, 0.00000000, 0.00000000, 0.00000000, 0.00000000, 0.74501799, 0.72975303, 0.78071397, 1.00000000]]
-)
-
-# initial guess is x_0 = 0
-x = np.zeros(10)
-
-
-def CGSolver(A, b, x, tol):
     # initial conditions
     r = b - np.dot(A, x)
     v = r
     rho = np.linalg.norm(r)
+    i = 0
     
     # runs while the error is greater than specified tolerance
-    while np.sqrt(rho) > tol:
+    while np.sqrt(rho) > tol and i < itMax:
         w = np.dot(A, v) # this is the operation that would be done in the memristive array
         alpha = rho / np.dot(v, w)
 
@@ -46,9 +33,5 @@ def CGSolver(A, b, x, tol):
         rhoNew = np.dot(r, r)
         v = r + (rhoNew/rho)*v
         rho = rhoNew
+        i += 1
     return x
-
-y = CGSolver(A,b,x,tol)
-
-print("the output guess vector is " + str(y))
-print("checking with the matrix A gives " + str(A @ y))
